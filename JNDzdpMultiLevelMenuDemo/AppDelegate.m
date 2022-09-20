ef01895bed32b1d142d2b456348572d4a69ce6cd
jiangneng <331385257@qq.com>
2022-09-20 23:31:26 +0800

    feature:- 代码提交。

--- JNDzdpMultiLevelMenuDemo/AppDelegate.m
+++ JNDzdpMultiLevelMenuDemo/AppDelegate.m
@@ -18,10 +18,22 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+        
+    [self getConfig];
     
     self.window.rootViewController = [[ViewController alloc] init];
     [self.window makeKeyAndVisible];
+    
+
     return YES;
+}
+- (void)getConfig {
+    [self getHostUrl];
+}
+- (void)getHostUrl {
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"Info" ofType:@"plist"];
+    NSDictionary *info = [[NSDictionary alloc] initWithContentsOfFile:path];
+    NSLog(@"当前的HOST_URL:%@",info[@"HOST_URL"]);
 }
 
 
